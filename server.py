6cf36e84240f93361cbf85beb873b411db3c684c
Jesaiah <jesaiahprayor@gmail.com>
2020-01-12 01:03:32 -0500

    Working database

--- server.py
+++ server.py
@@ -1,9 +1,19 @@
 from flask import Flask 
 from flask import request
+from flask_pymongo import PyMongo
+
 app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb://jj:jems101@ds139934.mlab.com:39934/escape-db?retryWrites=false"
+mongo = PyMongo(app)
+
+record = {
+    "hi": "JJ"
+}
 
 @app.route('/')
 def index():
+    mongo.db.users.insert_one(record)
     return '<h1> Hello JJ </h1>'
 
 @app.route('/user/<name>')
