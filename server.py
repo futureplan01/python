87208242ffc523bce10df92139eac95f6ff806ed
Jesaiah <jesaiahprayor@gmail.com>
2020-01-12 04:05:34 -0500

    Organized Events

--- server.py
+++ server.py
@@ -1,19 +1,61 @@
 from flask import Flask 
 from flask import request
 from flask_pymongo import PyMongo
+import sys
+import os
 
 app = Flask(__name__)
 
 app.config["MONGO_URI"] = "mongodb://jj:jems101@ds139934.mlab.com:39934/escape-db?retryWrites=false"
 mongo = PyMongo(app)
 
-record = {
-    "hi": "JJ"
+EventRecord = {
+    "Category": None,
+    "Country" : None,
+    "Address" : None,
+    "LatLong" : None,
+    "Event" : None,    
+    "Days" : None,
+    "Hours": None,
+    "Price" : None,
+    "Phone Number": None,
+    "Booking Url" : None,
+    "Email": None,
 }
+filename = sys.argv[1]
+
+def placeInDataBase (filename):
+
+    if not os.path.isfile(filename):
+        print("File Path {} does not exist. Exiting... ".format(filename))
+        sys.exit()
+    else:
+        file = open(filename)
+        line = file.readline()
+        count = 1
+        while line:
+            EventList = line.split("|")
+            EventRecord["Category"] = EventList[0]
+            EventRecord["Country"]  = EventList[1]
+            EventRecord["Address"]  = EventList[2]
+            EventRecord["LatLong"]  = EventList[3]
+            EventRecord["Event"]    = EventList[4]
+            EventRecord["Days"]     = EventList[5]
+            EventRecord["Hours"]    = EventList[6]
+            EventRecord["Price"]    = EventList[7]
+            EventRecord["Phone"]    = EventList[8]
+            EventRecord["BookingUrl"] = EventList[9]
+            EventRecord["Email"]      = EventList[10]
+            print(EventRecord)
+            line = file.readline()
+            count+=1
+
+        file.close()
+        print("read file succesfully!")
 
 @app.route('/')
 def index():
-    mongo.db.users.insert_one(record)
+    placeInDataBase(filename)
     return '<h1> Hello JJ </h1>'
 
 @app.route('/user/<name>')
