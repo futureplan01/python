004e208060781d846a9f1774f25ecd87d2d99543
Jesaiah <jesaiahprayor@gmail.com>
2020-01-14 02:20:14 -0500

    Added cors

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 import db
 import sys
 import os
 import copy
 
 app = Flask(__name__)
+CORS(app)
 
 EventRecord = {
     "Category": None,
